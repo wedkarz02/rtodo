81fb15883aa42051642ad4d7e9e40fcfe33d8241
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2023-11-05 20:38:04 +0100

    testing clap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
-use clap::Parser;
+// This is an example rewritten from the clap docs.
+// https://docs.rs/clap/latest/clap/_derive/_tutorial/chapter_0/index.html
+
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(short, long)]
-    asdf: String,
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// does test thingy
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
 }
 
 fn main() {
